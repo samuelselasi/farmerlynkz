dd365e7c846b46774346479a9e853c27841af535
sel <kirssymoore@gmail.com>
2021-02-26 15:25:07 +0000

    deadline

--- routers/appraiser/crud.py
+++ routers/appraiser/crud.py
@@ -6,3 +6,13 @@ from fastapi import Depends
 async def read_appraiser_appraisees(user_id, db: Session):
     res = db.execute(""" select get_list_of_appraisee(:user_id) """,{'user_id':user_id})
     return res.fetchall()
+
+async def read_completed_list(user_id:int, deadline:str, db:Session):
+    res = db.execute("""SELECT public.get_list_of_completed_form(:deadline, :user_id)""",{'user_id':user_id, 'deadline':deadline})
+    res = res.fetchall()
+    return res    
+
+async def read_incomplete_list(user_id:int, deadline:str, db:Session):
+    res = db.execute("""SELECT public.get_list_of_incomplete_form(:deadline, :user_id)""",{'user_id':user_id, 'deadline':deadline})
+    res = res.fetchall()
+    return res    
--- routers/appraiser/main.py
+++ routers/appraiser/main.py
@@ -12,3 +12,11 @@ router = APIRouter()
 @router.get("/Appraisees/{user_id}/")
 async def read_appraiser_appraisees(user_id:int, db: Session = Depends(get_db)):
     return await crud.read_appraiser_appraisees(user_id, db)
+
+@router.get("/completedlist/{user_id}/{deadline}")
+async def read_completed_list(user_id:int, deadline:str, db: Session = Depends(get_db)):
+    return await crud.read_completed_list(user_id, deadline, db)
+
+@router.get("/incompletelist/{user_id}/{deadline}")
+async def read_incomplete_list(user_id:int, deadline:str, db: Session = Depends(get_db)):
+    return await crud.read_incomplete_list(user_id, deadline, db)
