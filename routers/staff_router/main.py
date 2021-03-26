7c3782f441f6b0efaf4accd038b0e429351b8f62
sel <kirssymoore@gmail.com>
2021-03-26 20:33:07 +0000

    approve fix

--- routers/staff_router/main.py
+++ routers/staff_router/main.py
@@ -31,15 +31,15 @@ async def read_deadline_table(db: Session = Depends(get_db)):
     return await crud.read_deadline_table(db)
 
 @router.get("/deadline/start/")
-async def read_start_deadline_table(db: Session = Depends(get_db)):
+async def read_start_deadline(db: Session = Depends(get_db)):
     return await crud.read_start_deadline_table(db)
 
 @router.get("/deadline/mid/")
-async def read_mid_deadline_table(db: Session = Depends(get_db)):
+async def read_mid_deadline(db: Session = Depends(get_db)):
     return await crud.read_mid_deadline_table(db)
 
 @router.get("/deadline/end/")
-async def read_end_deadline_table(db: Session = Depends(get_db)):
+async def read_end_deadline(db: Session = Depends(get_db)):
     return await crud.read_end_deadline_table(db)
 
 
