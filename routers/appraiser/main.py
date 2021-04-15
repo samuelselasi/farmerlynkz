e960e91accb0840e2742a204811ea032aa32d102
sel <kirssymoore@gmail.com>
2021-04-15 16:53:30 +0000

    token

--- routers/appraiser/main.py
+++ routers/appraiser/main.py
@@ -11,20 +11,20 @@ router = APIRouter()
 
 
 # GET APPRAISER DETAILS
-@router.get("/Appraiseesopen/{user_id}/")
-async def read_appraiser_appraisees(user_id:int, db:Session=Depends(get_db)):
-    return await crud.read_appraiser_appraisees(user_id, db)
+# @router.get("/Appraiseesopen/{user_id}/")
+# async def read_appraiser_appraisees(user_id:int, db:Session=Depends(get_db)):
+#     return await crud.read_appraiser_appraisees(user_id, db)
 
 @router.get("/Appraisees/{user_id}/")
-async def read_appraiser_appraisees(user_id:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_appraiser_appraisees(user_id:int, token:str, db:Session=Depends(get_db)):
     return await crud.read_appraiser_appraisees_auth(user_id, token, db)
 
-@router.get("/approvedopen/start/")
-async def read_approved_forms(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
-    return await crud.read_approved_forms( deadline, user_id, db)
+# @router.get("/approvedopen/start/")
+# async def read_approved_forms(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
+#     return await crud.read_approved_forms( deadline, user_id, db)
 
 @router.get("/approved/start/")
-async def read_approved_forms(deadline='Start', user_id=1, token:str=Depends(oauth2_scheme),  db:Session=Depends(get_db)):
+async def read_approved_forms(token:str, deadline='Start', user_id=1, db:Session=Depends(get_db)):
     return await crud.read_approved_forms_auth( deadline, user_id, token, db)
 
 @router.get("/approved/mid/")
@@ -35,12 +35,12 @@ async def read_approved_forms(deadline='Mid', user_id=1,  db:Session=Depends(get
 async def read_approved_forms(deadline='End', user_id=1,  db:Session=Depends(get_db)):
     return await crud.read_approved_forms( deadline, user_id, db)
 
-@router.get("/completedlistopen/start/")
-async def read_completed_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
-    return await crud.read_completed_list( deadline, user_id, db)
+# @router.get("/completedlistopen/start/")
+# async def read_completed_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
+#     return await crud.read_completed_list( deadline, user_id, db)
 
 @router.get("/completedlist/start/")
-async def read_completed_list(deadline='Start', user_id=1, token:str=Depends(oauth2_scheme),  db:Session=Depends(get_db)):
+async def read_completed_list(token:str, deadline='Start', user_id=1, db:Session=Depends(get_db)):
     return await crud.read_completed_list_auth( deadline, user_id, token, db)
 
 @router.get("/completedlist/mid/")
@@ -51,12 +51,12 @@ async def read_completed_list(deadline='Mid', user_id=1,  db:Session=Depends(get
 async def read_completed_list(deadline='End', user_id=1,  db:Session=Depends(get_db)):
     return await crud.read_completed_list( deadline, user_id, db)
 
-@router.get("/waitingapprovalopen/start/")
-async def waiting_approval_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
-    return await crud.waiting_approval_list( deadline, user_id, db)
+# @router.get("/waitingapprovalopen/start/")
+# async def waiting_approval_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
+#     return await crud.waiting_approval_list( deadline, user_id, db)
 
 @router.get("/waitingapproval/start/")
-async def waiting_approval_list(deadline='Start', user_id=1, token:str=Depends(oauth2_scheme),  db:Session=Depends(get_db)):
+async def waiting_approval_list(token:str, deadline='Start', user_id=1, db:Session=Depends(get_db)):
     return await crud.waiting_approval_list_auth( deadline, user_id, token, db)
 
 @router.get("/waitingapproval/mid/")
@@ -64,19 +64,19 @@ async def waiting_approval_list(deadline='Mid', user_id=1,  db:Session=Depends(g
     return await crud.waiting_approval_list( deadline, user_id, db)
 
 @router.get("/waitingapprovalauth/mid/")
-async def waiting_approval_list_auth(deadline='Mid', user_id=1, token:str=Depends(oauth2_scheme),  db:Session=Depends(get_db)):
+async def waiting_approval_list_auth(token:str, deadline='Mid', user_id=1, db:Session=Depends(get_db)):
     return await crud.waiting_approval_list_auth( deadline, user_id, token, db)
 
 @router.get("/waitingapproval/end/")
 async def waiting_approval_list(deadline='End', user_id=1,  db:Session=Depends(get_db)):
     return await crud.waiting_approval_list( deadline, user_id, db)
 
-@router.get("/incompletedlistopen/start/")
-async def read_incompleted_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
-    return await crud.read_incomplete_list( deadline, user_id, db)   
+# @router.get("/incompletedlistopen/start/")
+# async def read_incompleted_list(deadline='Start', user_id=1,  db:Session=Depends(get_db)):
+#     return await crud.read_incomplete_list( deadline, user_id, db)   
 
 @router.get("/incompletedlist/start/")
-async def read_incompleted_list(deadline='Start', user_id=1, token:str=Depends(oauth2_scheme),  db:Session=Depends(get_db)):
+async def read_incompleted_list(token:str, deadline='Start', user_id=1, db:Session=Depends(get_db)):
     return await crud.read_incomplete_list_auth( deadline, user_id, token, db) 
 
 @router.get("/incompletedlist/mid/")
@@ -87,20 +87,20 @@ async def read_incompleted_list(deadline='Mid', user_id=1,  db:Session=Depends(g
 async def read_incompleted_list(deadline='End', user_id=1,  db:Session=Depends(get_db)):
     return await crud.read_incomplete_list( deadline, user_id, db)  
 
-@router.get("/deadlineopen/")
-async def read_deadline_table(db:Session=Depends(get_db)):
-    return await crud.read_deadline_table(db)
+# @router.get("/deadlineopen/")
+# async def read_deadline_table(db:Session=Depends(get_db)):
+#     return await crud.read_deadline_table(db)
 
 @router.get("/deadline/")
-async def read_deadline_table(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_deadline_table(token:str, db:Session=Depends(get_db)):
     return await crud.read_deadline_table_auth(token, db)
 
-@router.get("/deadlineopen/start/")
-async def read_start_deadline(db:Session=Depends(get_db)):
-    return await crud.read_start_deadline_table(db)
+# @router.get("/deadlineopen/start/")
+# async def read_start_deadline(db:Session=Depends(get_db)):
+#     return await crud.read_start_deadline_table(db)
 
 @router.get("/deadline/start/")
-async def read_start_deadline(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_start_deadline(token:str, db:Session=Depends(get_db)):
     return await crud.read_start_deadline_table_auth(token, db)
 
 @router.get("/deadline/mid/")
@@ -111,58 +111,58 @@ async def read_mid_deadline(db:Session=Depends(get_db)):
 async def read_end_deadline(db:Session=Depends(get_db)):
     return await crud.read_end_deadline_table(db)
 
-@router.get("/supervisorsopen/")
-async def read_supervisors(db:Session=Depends(get_db)):
-    return await crud.read_supervisors(db)
+# @router.get("/supervisorsopen/")
+# async def read_supervisors(db:Session=Depends(get_db)):
+#     return await crud.read_supervisors(db)
 
 @router.get("/supervisors/")
-async def read_supervisors(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_supervisors(token:str, db:Session=Depends(get_db)):
     return await crud.read_supervisors_auth(token, db)
 
-@router.get("/yearlyformdetailsopen/{staff_id}/{form_year}")
-async def read_yearly_form_deatails(staff_id:int, form_year:int, db:Session=Depends(get_db)):
-    return await crud.read_yearly_form_deatails(staff_id, form_year, db)
+# @router.get("/yearlyformdetailsopen/{staff_id}/{form_year}")
+# async def read_yearly_form_deatails(staff_id:int, form_year:int, db:Session=Depends(get_db)):
+#     return await crud.read_yearly_form_deatails(staff_id, form_year, db)
 
 @router.get("/yearlyformdetails/{staff_id}/{form_year}")
-async def read_yearly_form_deatails(staff_id:int, form_year:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_yearly_form_deatails(staff_id:int, form_year:int, token:str, db:Session=Depends(get_db)):
     return await crud.read_yearly_form_deatails_auth(staff_id, form_year, token, db)
 
 
 # APPROVE FORM DETAILS
-@router.get("/approveformopen/{appraisal_form_id}/")
-async def approve_form(appraisal_form_id:int, type_form='Start', db:Session=Depends(get_db)):
-    return await crud.approve_form(appraisal_form_id, type_form, dbs)
+# @router.get("/approveformopen/{appraisal_form_id}/")
+# async def approve_form(appraisal_form_id:int, type_form='Start', db:Session=Depends(get_db)):
+#     return await crud.approve_form(appraisal_form_id, type_form, dbs)
 
 @router.get("/approveform/{appraisal_form_id}/")
-async def approve_form_details(appraisal_form_id:int, type_form='Start', token: str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def approve_form_details(token:str, appraisal_form_id:int, type_form='Start', db:Session=Depends(get_db)):
     return await crud.approve_form_details_auth(appraisal_form_id, type_form, token, db)
 
 
 # CREATE APPRAISER DETAILS
-@router.post("/deadlineopen/")
-async def create_deadline(payload:schemas.create_deadline, db:Session=Depends(get_db)):
-    return await crud.create_deadline(payload.deadline_type, payload.start_date, payload.ending, db)
+# @router.post("/deadlineopen/")
+# async def create_deadline(payload:schemas.create_deadline, db:Session=Depends(get_db)):
+#     return await crud.create_deadline(payload.deadline_type, payload.start_date, payload.ending, db)
 
 @router.post("/deadline/")
-async def create_deadline(payload:schemas.create_deadline, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def create_deadline(payload:schemas.create_deadline, token:str, db:Session=Depends(get_db)):
     return await crud.create_deadline_auth(payload.deadline_type, payload.start_date, payload.ending, token, db)
 
 
 # UPDATE APPRAISER DETAILS
-@router.put("/deadlineopen/")
-async def update_deadline_table(deadline:schemas.update_deadline, db:Session=Depends(get_db)):
-    return await crud.update_deadline(deadline, db)
+# @router.put("/deadlineopen/")
+# async def update_deadline_table(deadline:schemas.update_deadline, db:Session=Depends(get_db)):
+#     return await crud.update_deadline(deadline, db)
 
 @router.put("/deadline/")
-async def update_deadline_table(deadline:schemas.update_deadline, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def update_deadline_table(deadline:schemas.update_deadline, token:str, db:Session=Depends(get_db)):
     return await crud.update_deadline_auth(deadline, token, db)
 
 
 # DELETE APPRAISER DETAILS
-@router.delete("/deadlineopen/{deadline_id}/")
-async def delete_deadline(deadline_id:int, db:Session=Depends(get_db)):
-    return await crud.delete_deadline(deadline_id, db)    
+# @router.delete("/deadlineopen/{deadline_id}/")
+# async def delete_deadline(deadline_id:int, db:Session=Depends(get_db)):
+#     return await crud.delete_deadline(deadline_id, db)    
 
 @router.delete("/deadline/{deadline_id}/")
-async def delete_deadline(deadline_id:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def delete_deadline(deadline_id:int, token:str, db:Session=Depends(get_db)):
     return await crud.delete_deadline_auth(deadline_id, token, db) 
--- routers/staff_router/main.py
+++ routers/staff_router/main.py
@@ -11,52 +11,52 @@ from uuid import UUID
 router = APIRouter()
 
 # READ STAFF DETAILS
-@router.get("/staffopen/")
-async def read_staff(db:Session=Depends(get_db)):
-    return await crud.read_staff(db)
+# @router.get("/staffopen/")
+# async def read_staff(db:Session=Depends(get_db)):
+#     return await crud.read_staff(db)
 
 @router.get("/staff/")
-async def read_staff(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_staff(token:str, db:Session=Depends(get_db)):
     return await crud.read_staff_auth(token, db)
 
-@router.get("/supervisorsopen/")
-async def read_supervisors(db:Session=Depends(get_db)):
-    return await crud.read_supervisors(db)
+# @router.get("/supervisorsopen/")
+# async def read_supervisors(db:Session=Depends(get_db)):
+#     return await crud.read_supervisors(db)
 
 @router.get("/supervisors/")
-async def read_supervisors(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_supervisors(token:str, db:Session=Depends(get_db)):
     return await crud.read_supervisors_auth(token, db)
 
-@router.get("/{name}open/")
-async def read_staff_by_name(name:str, db:Session=Depends(get_db)):
-    return await crud.read_staff_by_name(name, db)
+# @router.get("/{name}open/")
+# async def read_staff_by_name(name:str, db:Session=Depends(get_db)):
+#     return await crud.read_staff_by_name(name, db)
 
 @router.get("/{name}/")
-async def read_staff_by_name(name:str, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_staff_by_name(name:str, token:str, db:Session=Depends(get_db)):
     return await crud.read_staff_by_name_auth(name, token, db)
 
-@router.get("/rolesopen/")
-async def read_roles(db:Session=Depends(get_db)):
-    return await crud.read_roles(db)
+# @router.get("/rolesopen/")
+# async def read_roles(db:Session=Depends(get_db)):
+#     return await crud.read_roles(db)
 
 @router.get("/roles/")
-async def read_roles(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_roles(token:str, db:Session=Depends(get_db)):
     return await crud.read_roles_auth(token, db)
 
-@router.get("/deadlineopen/")
-async def read_deadline_table(db:Session=Depends(get_db)):
-    return await crud.read_deadline_table(db)
+# @router.get("/deadlineopen/")
+# async def read_deadline_table(db:Session=Depends(get_db)):
+#     return await crud.read_deadline_table(db)
 
 @router.get("/deadline/")
-async def read_deadline_table(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def read_deadline_table(token:str, db:Session=Depends(get_db)):
     return await crud.read_deadline_table_auth(token, db)
 
-@router.get("/deadlinestartopen/")
-async def read_start_deadline(db:Session=Depends(get_db)):
-    return await crud.read_start_deadline_table(db)
+# @router.get("/deadlineopen/start/")
+# async def read_start_deadline(db:Session=Depends(get_db)):
+#     return await crud.read_start_deadline_table(db)
 
-@router.get("/deadlinestart/")
-async def read_start_deadline(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+@router.get("/deadline/start/")
+async def read_start_deadline(token:str, db:Session=Depends(get_db)):
     return await crud.read_start_deadline_table_auth(token, db)
 
 @router.get("/deadline/mid/")
@@ -69,88 +69,88 @@ async def read_end_deadline(db:Session=Depends(get_db)):
 
 
 # DEACTIVATE STAFF 
-@router.get("/deactivatestaff/{staff_id}/open/")
-async def deactivate_staff(staff_id:int, db:Session=Depends(get_db)):
-    return await crud.deactivate_staff(staff_id, db)
+# @router.get("/deactivatestaff/{staff_id}/open/")
+# async def deactivate_staff(staff_id:int, db:Session=Depends(get_db)):
+#     return await crud.deactivate_staff(staff_id, db)
 
 @router.get("/deactivatestaff/{staff_id}/")
-async def deactivate_staff(staff_id:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def deactivate_staff(staff_id:int, token:str, db:Session=Depends(get_db)):
     return await crud.deactivate_staff_auth(staff_id, token, db)
 
 
 # CREATE STAFF DETAILS
-@router.post("/staffopen/")
-async def create_staff(payload:schemas.UserCreate, db:Session=Depends(get_db)):
-    return await crud.create_staff(payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles, db)
+# @router.post("/staffopen/")
+# async def create_staff(payload:schemas.UserCreate, db:Session=Depends(get_db)):
+#     return await crud.create_staff(payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles, db)
 
 @router.post("/staff/")
-async def create_staff(payload:schemas.UserCreate, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def create_staff(payload:schemas.UserCreate, token:str, db:Session=Depends(get_db)):
     return await crud.create_staff_auth(payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles, token, db)
 
-@router.post("/rolesopen/")
-async def create_roles(payload:schemas.create_roles, db:Session=Depends(get_db)):
-    return await crud.create_roles(payload.role_description, db)
+# @router.post("/rolesopen/")
+# async def create_roles(payload:schemas.create_roles, db:Session=Depends(get_db)):
+#     return await crud.create_roles(payload.role_description, db)
 
 @router.post("/roles/")
-async def create_roles(payload:schemas.create_roles, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def create_roles(payload:schemas.create_roles, token:str, db:Session=Depends(get_db)):
     return await crud.create_roles_auth(payload.role_description, token, db)
 
-@router.post("/deadlineopen/")
-async def create_deadline(payload:schemas.create_deadline, db:Session=Depends(get_db) ):
-    return await crud.create_deadline(payload.deadline_type, payload.start_date, payload.ending, db)
+# @router.post("/deadlineopen/")
+# async def create_deadline(payload:schemas.create_deadline, db:Session=Depends(get_db) ):
+#     return await crud.create_deadline(payload.deadline_type, payload.start_date, payload.ending, db)
 
 @router.post("/deadline/")
-async def create_deadline(payload:schemas.create_deadline, token:str=Depends(oauth2_scheme), db: Session=Depends(get_db) ):
+async def create_deadline(payload:schemas.create_deadline, token:str, db: Session=Depends(get_db) ):
     return await crud.create_deadline_auth(payload.deadline_type, payload.start_date, payload.ending, token, db)
 
 
 # UPDATE STAFF DETAILS
-@router.put("/staffopen/")
-async def update_staff(payload:schemas.update_staff, db:Session=Depends(get_db)):
-    return await crud.update_staff(payload.staff_id, payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles,  db)
+# @router.put("/staffopen/")
+# async def update_staff(payload:schemas.update_staff, db:Session=Depends(get_db)):
+#     return await crud.update_staff(payload.staff_id, payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles,  db)
 
 @router.put("/staff/")
-async def update_staff(payload:schemas.update_staff, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def update_staff(payload:schemas.update_staff, token:str, db:Session=Depends(get_db)):
     return await crud.update_staff_auth(payload.staff_id, payload.fname, payload.sname, payload.oname, payload.email, payload.supervisor, payload.gender, payload.department, payload.positions, payload.grade, payload.appointment, payload.roles, token, db)
 
-@router.put("/Deadlineopen/")
-async def update_deadline_table(deadline:schemas.update_deadline, db:Session=Depends(get_db)):
-    return await crud.update_deadline(deadline, db)
+# @router.put("/Deadlineopen/")
+# async def update_deadline_table(deadline:schemas.update_deadline, db:Session=Depends(get_db)):
+#     return await crud.update_deadline(deadline, db)
 
 @router.put("/Deadline/")
-async def update_deadline_table(deadline:schemas.update_deadline, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def update_deadline_table(deadline:schemas.update_deadline, token:str, db:Session=Depends(get_db)):
     return await crud.update_deadline_auth(deadline, token, db)
 
-@router.put("/rolesopen/")
-async def update_roles(payload:schemas.update_roles, db:Session=Depends(get_db)):
-    return await crud.update_roles(payload.role_id, payload.role_description, db)
+# @router.put("/rolesopen/")
+# async def update_roles(payload:schemas.update_roles, db:Session=Depends(get_db)):
+#     return await crud.update_roles(payload.role_id, payload.role_description, db)
 
 @router.put("/roles/")
-async def update_roles(payload:schemas.update_roles, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def update_roles(payload:schemas.update_roles, token:str, db:Session=Depends(get_db)):
     return await crud.update_roles_auth(payload.role_id, payload.role_description, token, db)
 
 
 # DELETE STAFF DETAILS
-@router.delete("/{staff_id}/open/")
-async def delete_staff(staff_id:int, db:Session=Depends(get_db)):
-    return await crud.delete_staff(staff_id, db)
+# @router.delete("/{staff_id}/open/")
+# async def delete_staff(staff_id:int, db:Session=Depends(get_db)):
+#     return await crud.delete_staff(staff_id, db)
 
 @router.delete("/{staff_id}/")
-async def delete_staff(staff_id:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def delete_staff(staff_id:int, token:str, db:Session=Depends(get_db)):
     return await crud.delete_staff_auth(staff_id, token, db)
 
-@router.delete("/{role_id}/open")
-async def delete_roles(role_id:int, db:Session=Depends(get_db)):
-    return await crud.delete_roles(role_id, db)
+# @router.delete("/{role_id}/open")
+# async def delete_roles(role_id:int, db:Session=Depends(get_db)):
+#     return await crud.delete_roles(role_id, db)
 
 @router.delete("/{role_id}/")
-async def delete_roles(role_id:int, tken:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def delete_roles(role_id:int, tken:str, db:Session=Depends(get_db)):
     return await crud.delete_roles_auth(role_id, token, db)
 
-@router.delete("/deadline/{deadline_id}/open/")
-async def delete_deadline(deadline_id:int, db:Session=Depends(get_db)):
-    return await crud.delete_deadline(deadline_id, db)    
+# @router.delete("/deadline/{deadline_id}/open/")
+# async def delete_deadline(deadline_id:int, db:Session=Depends(get_db)):
+#     return await crud.delete_deadline(deadline_id, db)    
 
 @router.delete("/deadline/{deadline_id}/")
-async def delete_deadline(deadline_id:int, token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+async def delete_deadline(deadline_id:int, token:str, db:Session=Depends(get_db)):
     return await crud.delete_deadline_auth(deadline_id, token, db) 
