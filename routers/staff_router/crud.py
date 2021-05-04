02d4b74b4736dc94d88a34cf35977bf1ffcd069b
sel <kirssymoore@gmail.com>
2021-05-04 07:56:23 +0000

    reactivate staff

--- routers/staff_router/crud.py
+++ routers/staff_router/crud.py
@@ -234,8 +234,7 @@ async def deactivate_staff_auth(staff_id:int, token:str, db:Session):
 
 # REACTIVATE STAFF
 async def reactivate_staff(staff_id:int, db:Session):
-    res = db.execute(""" UPDATE public.staff  SET staff_id=:staff_id, staff_status=1 WHERE staff_id=:staff_id """, {'staff_id': staff_id}) #CHANGE STAFF STATUS FROM 1 TO 0 USING ID
-    res = res.fetchall()
+    res = db.execute(""" UPDATE public.staff SET staff_status=1 WHERE staff_id=:staff_id """, {'staff_id': staff_id}) #CHANGE STAFF STATUS FROM 1 TO 0 USING ID
     db.commit()
     return res
 
